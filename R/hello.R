2a6d4ac644b02f5d0bd79c6a32d3af65f1864139
sohnijagga <sohnijagga2000@gmail.com>
2021-06-21 11:37:48 +0530

    Task#1

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,24 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+#Import the data from your system from directory
+setwd("D:/")
+`GRIPdsda#1`<- read.csv("GRIPdsda#1.csv")
+print(`GRIPdsda#1`) #This command will show your data in Console.
 
-hello <- function() {
-  print("Hello, world!")
-}
+#Data Exploration
+head(`GRIPdsda#1`) #This will give you the initital entries of data
+tail(`GRIPdsda#1`) #This will give you last entries of data
+summary(`GRIPdsda#1`)
+cor(`GRIPdsda#1`)
+#This shows higher correlation between hours and scores
+
+#Data Visualization
+attach(`GRIPdsda#1`)
+regression<- lm(Scores ~ ï..Hours)
+summary(regression)
+plot(ï..Hours,Scores, main= "Hours vs Scores", xlab="Hours", ylab="Scores" )
+abline(regression, col= "red")
+lm(`GRIPdsda#1`)
+
+#Prediction
+predictscore<-data.frame(ï..Hours=9.25)
+predict(regression,predictscore)
+#This shows the predicted score of a student who studies 9.25 hour/day is 92.90)
